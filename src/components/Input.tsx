e1c26a6f9ef150748dbae48653ebdbe39065c180
Dmitry <dima.matatoff@yandex.ru>
2021-06-14 12:50:59 +0300

    Fix prettier

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,29 @@
-import React, {useState, FC} from "react";
-import {TextField, Button} from "@material-ui/core";
-import "./CommonStyles.css";
+import React, { useState, FC } from 'react'
+import { TextField, Button } from '@material-ui/core'
+import './CommonStyles.css'
 
 type propsType = { addTask: (task: string) => void }
 
 const CustomInput: FC<propsType> = (props) => {
-    const [inputValue, setInputValue] = useState("");
-    const addTaskHandler = () => {
-        props.addTask(inputValue);
-        setInputValue("")
-    };
-    return (
-        <div className="customInput">
-            <TextField value={inputValue} onChange={(e) => setInputValue(e.target.value)}
-                       id="outlined-basic" label="Outlined" variant="outlined"/>
-            <Button variant="contained" color="primary" onClick={addTaskHandler}>
-                Add task
-            </Button>
-        </div>
-    )
-};
+  const [inputValue, setInputValue] = useState('')
+  const addTaskHandler = () => {
+    props.addTask(inputValue)
+    setInputValue('')
+  }
+  return (
+    <div className="customInput">
+      <TextField
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+      />
+      <Button variant="contained" color="primary" onClick={addTaskHandler}>
+        Add task
+      </Button>
+    </div>
+  )
+}
 
-
-export default CustomInput;
+export default CustomInput
--- src/components/Todo-list-items/Todo-item.tsx
+++ src/components/Todo-list-items/Todo-item.tsx
@@ -1,31 +1,41 @@
-import React from "react";
-import "./Todo.css"
-import {Button} from "@material-ui/core";
+import React from 'react'
+import './Todo.css'
+import { Button } from '@material-ui/core'
 
 interface propType {
-    id: number
-    task: string
-    completed: boolean
-    important: boolean
-    onDelete: (id: number) => void
-    onCompletedHandler: (completed: boolean, id: number) => void
-    onImportantHandler: (important: boolean, id: number) => void
+  id: number
+  task: string
+  completed: boolean
+  important: boolean
+  onDelete: (id: number) => void
+  onCompletedHandler: (completed: boolean, id: number) => void
+  onImportantHandler: (important: boolean, id: number) => void
 }
 
 export default class TodoItem extends React.Component<propType> {
-    render() {
-        const {completed, important, id, task, onCompletedHandler, onImportantHandler, onDelete} = this.props;
-        return (
-            <>
-                <li className={`item-list
-                ${completed ? "completed" : null} 
-                ${important ? "important" : null}`}
-                    onClick={() => onCompletedHandler(completed, id)}>
-                    {task}
-                </li>
-                <Button onClick={() => onDelete(id)}>-</Button>
-                <Button onClick={() => onImportantHandler(important, id)}>!</Button>
-            </>
-        )
-    }
-};
+  render() {
+    const {
+      completed,
+      important,
+      id,
+      task,
+      onCompletedHandler,
+      onImportantHandler,
+      onDelete,
+    } = this.props
+    return (
+      <>
+        <li
+          className={`item-list
+                ${completed ? 'completed' : null} 
+                ${important ? 'important' : null}`}
+          onClick={() => onCompletedHandler(completed, id)}
+        >
+          {task}
+        </li>
+        <Button onClick={() => onDelete(id)}>-</Button>
+        <Button onClick={() => onImportantHandler(important, id)}>!</Button>
+      </>
+    )
+  }
+}
