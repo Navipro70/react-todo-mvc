1fec6bf0f47c93dc22031cd3d155b99ac7dfe52e
Dmitry Matatov <dima.matatoff@yandex.ru>
2021-09-14 01:47:00 +0300

    Updates

---
+++ precache-manifest.9b348fb9c12b64e1c90a92a8de1b1b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de3eb107ad6e13d3a37621b8fb07f62",
+    "url": "/react-todo-mvc/index.html"
+  },
+  {
+    "revision": "001eb7de12b261a4b587",
+    "url": "/react-todo-mvc/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "50cc33531d6a47376a92",
+    "url": "/react-todo-mvc/static/css/main.d2b92b3c.chunk.css"
+  },
+  {
+    "revision": "001eb7de12b261a4b587",
+    "url": "/react-todo-mvc/static/js/2.4ac3ab5b.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-todo-mvc/static/js/2.4ac3ab5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cc33531d6a47376a92",
+    "url": "/react-todo-mvc/static/js/main.4a8cf8bb.chunk.js"
+  },
+  {
+    "revision": "e3b405c89bffd69543f7",
+    "url": "/react-todo-mvc/static/js/runtime-main.9614e6f3.js"
+  }
+]);
